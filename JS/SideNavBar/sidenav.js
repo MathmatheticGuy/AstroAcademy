a79f5cff18b9e5cc4855b0670551c2274a7984ce
MathmatheticGuy <nhatthanhdinh482@gmail.com>
2023-11-03 22:33:57 +0700

    data cleansing

--- JS/SideNavBar/sidenav.js
+++
@@ -1,6 +0,0 @@
-const toggle =  document.querySelector(".sidebar-toggle-btn");
-const sidebar = document.querySelector(".sidebar");
-
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
